0dea245a4ab8621773efd3b1132ad056ec398f6a
Jatin Gupta <guptajatin@outlook.com>
2020-06-24 14:43:46 +1000

    update before question

--- lab2/lab.py
+++ lab2/lab.py
@@ -1,8 +1,10 @@
+import imutils
 # Task1 Hint: (with sample code for the SIFT detector)
 # Initialize SIFT detector, detect keypoints, store and show SIFT keypoints of original image in a Numpy array
 # Define parameters for SIFT initializations such that we find only 10% of keypoints
 import cv2
 import matplotlib.pyplot as plt
+
 
 class SiftDetector():
     def __init__(self, norm="L2", params=None):
@@ -61,43 +63,50 @@ def get_img_center(image):
     center = height // 2, width // 2
 
     return center
-img = cv2.imread('COMP9517_20T2_Lab2_Image.jpg')
-def task1_a(img, name):
-    gray= cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
+
+img = cv2.imread('COMP9517_20T2_Lab2_Image.jpg')
+img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+
+def task1_a(img, name):
     sift = SiftDetector()
     detector = sift.get_detector(None)
     
-    kp = detector.detect(gray, None)
-    img = cv2.drawKeypoints(gray,kp,img)
+    kp = detector.detect(img, None)
+    img = cv2.drawKeypoints(img,kp,img)
     cv2.imwrite(name, img)
+
 
 # task1_a(img.copy(), 'task1_A.jpg')
+
+
 def task1_b(img, name):
     
-    gray= cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    detector = get_sift_detector()
 
-    sift = SiftDetector()
-    
-    params={}
-    params["n_features"]=0
-    params["n_octave_layers"]=3
-    params["contrast_threshold"]=0.136
-    params["edge_threshold"]=10
-    params["sigma"]=1.6
-
-    detector = sift.get_detector(params)
-    
-    kp = detector.detect(gray, None)
-    print(len(kp))
-    img = cv2.drawKeypoints(gray,kp, img)
+    kp = detector.detect(img, None)
+    img = cv2.drawKeypoints(img, kp, img)
     cv2.imwrite(name, img)
     return img
-    
+
+
+def get_sift_detector():
+    sift = SiftDetector()
+    params = {}
+    params["n_features"] = 0
+    params["n_octave_layers"] = 3
+    params["contrast_threshold"] = 0.136
+    params["edge_threshold"] = 10
+    params["sigma"] = 1.6
+    detector = sift.get_detector(params)
+    return detector
+
+
 # task1_b(img.copy(), 'task1_B.jpg')
 
 def task1_b_n(img, name):
-    gray= cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
 
     sift = SiftDetector()
     
@@ -110,60 +119,90 @@ def task1_b_n(img, name):
 
     detector = sift.get_detector(params)
     
-    kp = detector.detect(gray, None)
-    img = cv2.drawKeypoints(gray,kp, img)
+    kp = detector.detect(img, None)
+    img = cv2.drawKeypoints(img,kp, img)
     cv2.imwrite(name, img)
 # task1_b_n(img.copy(), 'task1_B_n.jpg')
 
 
-
 def task2(img, name):
     # part a
-    scale_percent = 115 # percent of original size
+    scale_percent = 115  # percent of original size
     width = int(img.shape[1] * scale_percent / 100)
     height = int(img.shape[0] * scale_percent / 100)
     dim = (width, height)
     # resize image
     resized = cv2.resize(img, dim)
-    
+
     # part b: increased to 710 from 625
-    resized_image = task1_b(resized, name+'_b.jpg')
+    resized_image = task1_b(resized, name + '_B.jpg')
 
     # part c
+    '''
+    Yes, the key points are roughly the same as SIFT is scale independent. It only looks at the texture of objects.
+    '''
 
     # part d
-    img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-    sift = SiftDetector()
-
-    params = {}
-    params["n_features"] = 0
-    params["n_octave_layers"] = 3
-    params["contrast_threshold"] = 0.136
-    params["edge_threshold"] = 10
-    params["sigma"] = 1.6
-
-    detector = sift.get_detector(params)
+    detector = get_sift_detector()
 
     # find the keypoints and descriptors with SIFT
     kp1, des1 = detector.detectAndCompute(img, None)
     kp2, des2 = detector.detectAndCompute(resized, None)
 
-
     # create BFMatcher object
-    bf = cv2.BFMatcher()
+    bf = cv2.BFMatcher(cv2.NORM_L2, crossCheck=False)
     matches = bf.knnMatch(des1, des2, k=2)
 
-    # # Apply ratio test
+    # Apply ratio test
     good = []
     for m, n in matches:
-        if m.distance < 0.95 * n.distance:
+        if m.distance < 0.75 * n.distance:
             good.append([m])
 
     good = sorted(good, key=lambda x: x[0].distance)
+
     # cv2.drawMatchesKnn expects list of lists as matches.
     img3 = cv2.drawMatchesKnn(img, kp1, resized, kp2, good[:5], img, flags=2)
-    cv2.imwrite(name+'_d.jpg', img3)
-    
-    
-    
-task2(img.copy(), 'task2')
+    cv2.imwrite(name + '_D.jpg', img3)
+
+
+# task2(img.copy(), 'task2')
+
+
+def task3(img, name):
+    # part a
+    rotated = imutils.rotate_bound(img, angle=60)
+
+    # part b
+    detector = get_sift_detector()
+    kp2, des2 = detector.detectAndCompute(rotated, None)
+
+    rotated_dist = cv2.drawKeypoints(rotated, kp2, rotated)
+    cv2.imwrite(name + '_B.jpg', rotated_dist)
+
+    # part c
+    '''
+    Yes, the key points are roughly the same as SIFT is orientation independent. It only looks at the texture of objects. But we can see little variations. 
+    '''
+
+    # part d
+    kp1, des1 = detector.detectAndCompute(img, None)
+
+    # create BFMatcher object
+    bf = cv2.BFMatcher(cv2.NORM_L2, crossCheck=False)
+    matches = bf.knnMatch(des1, des2, k=2)
+
+    # Apply ratio test
+    good = []
+    for m, n in matches:
+        if m.distance < 0.70 * n.distance:
+            good.append([m])
+
+    good = sorted(good, key=lambda x: x[0].distance)
+
+    # cv2.drawMatchesKnn expects list of lists as matches.
+    img3 = cv2.drawMatchesKnn(img, kp1, rotated, kp2, good[:5], img, flags=2)
+    cv2.imwrite(name + '_D.jpg', img3)
+
+
+task3(img.copy(), 'task3')
