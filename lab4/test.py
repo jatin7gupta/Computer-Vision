7d2f8c30e6261bbc02a8ddce94c51d9965a33ee4
Jatin Gupta <guptajatin@outlook.com>
2020-07-12 12:13:47 +1000

    Update test.py

--- lab4/test.py
+++ lab4/test.py
@@ -9,15 +9,17 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import confusion_matrix
 test_size = 0.25
 digits = load_digits()
-X_train, X_test, y_train, y_test = train_test_split(digits.data, digits.target, test_size=test_size)
+X_train, X_test, y_train, y_test = train_test_split(digits.data, digits.target, test_size=test_size, random_state=42)
 
 
-def knn():
-    neigh = KNeighborsClassifier(n_neighbors=3)
+def knn(n, plot=False):
+    neigh = KNeighborsClassifier(n_neighbors=n)
     neigh.fit(X_train, y_train)
     predict = neigh.predict(X_test)
     accuracy = round(neigh.score(X_test, y_test), 3)
     recall = round(metrics.recall_score(y_test, predict, average="micro"), 3)
+    if plot:
+        return accuracy
     return f'Accuracy: {accuracy}\t Recall: {recall}', confusion_matrix(y_test, predict)
 
 
@@ -41,10 +43,23 @@ def DT():
 
 print('COMP9517 Week 5 Lab - z5240221\n')
 print(f'Test size = {test_size}')
-knn_result, confusion_mat = knn()
+knn_result, confusion_mat = knn(n=5)
 print(f'KNN {knn_result}')
 print(f'SGD {SGD()}')
 print(f'DT {DT()}')
 print(f'\nKNN Confusion Matrix: \n{confusion_mat}')
+
+
+def plot():
+    x = []
+    y = []
+    for i in range(1, 20, 2):
+        knn_result = knn(i, plot=True)
+        x.append(i)
+        y.append(knn_result)
+    plt.plot(x, y, '-gD', markevery=True)
+    plt.xlabel('N')
+    plt.ylabel('Accuracy')
+    plt.show()
 
 
